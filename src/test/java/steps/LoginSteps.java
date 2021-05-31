efce35ac5078678a6ada2d84a28de149130ed2f1
cancanki <cankiosman@gmail.com>
2021-05-31 22:00:46 +0300

    Initial commit

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,44 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginSteps {
+
+    public static WebDriver driver;
+
+    @Given("^user is  on homepage$")
+    public void user_is_on_homepage() {
+        System.setProperty("webdriver.chrome.driver","web-drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @When("^user navigates to Login Page$")
+    public void user_navigates_to_Login_Page() throws Throwable {
+        driver.findElement(By.className("login")).click();
+    }
+
+    @When("^user enters username and Password$")
+    public void user_enters_username_and_Password() throws Throwable {
+        driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("passwd")).sendKeys("test");
+        driver.findElement(By.id("SubmitLogin")).click();
+    }
+
+    @Then("^success message is displayed$")
+    public void success_message_is_displayed() throws Throwable {
+        String exp_message = "Welcome to your account. Here you can manage all   of your personal information and orders.";
+        String actual = driver.findElement(By.cssSelector(".info-account")).getText();
+        Assert.assertEquals(exp_message, actual);
+        driver.quit();
+    }
+}
