569dd18942a583c54a6b0a5d18b993a6796eb91c
cancanki <cankiosman@gmail.com>
2022-05-15 01:10:03 +0300

    clean commit as version1

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,58 @@
+package utils;
+
+import org.openqa.selenium.By;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private Properties properties;
+    private final String propertyFilePath = "src/test/resources/configs/config.properties";
+
+
+    public ConfigReader() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+        }
+    }
+
+    public String getProperty(String propType) {
+        String property = properties.getProperty(propType);
+        if (property != null) return property;
+        else throw new RuntimeException("property not specified in the Configuration.properties file.");
+    }
+/*
+    public String getWebDriver() {
+        String wd = properties.getProperty("webdriver");
+        if(wd != null) return wd;
+        else throw new RuntimeException("webdriver not specified in the Configuration.properties file.");
+    }
+
+    public long getImplicitlyWait() {
+        String implicitlyWait = properties.getProperty("implicitlyWait");
+        if(implicitlyWait != null) return Long.parseLong(implicitlyWait);
+        else throw new RuntimeException("implicitlyWait not specified in the Configuration.properties file.");
+    }
+
+    public String getApplicationUrl() {
+        String url = properties.getProperty("url");
+        if(url != null) return url;
+        else throw new RuntimeException("url not specified in the Configuration.properties file.");
+    }
+*/
+}
